8a000a63386a0d6c5093cb3943f2fa96388ef4ba
max-konin <qwas666>
2013-07-09 16:30:22 +0700

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Masterpiece::Application.config.secret_token = '2bd784e8c42ad3cbe3064cb398230b9ac7f27c693f6bb855714ae25094d21f1acd00dcdaa0631ccc9c9c1b95ca2256cc5345ec6ba892fe0f5e22208e2669036b'
