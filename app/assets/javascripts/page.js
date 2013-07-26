cadf3f4627c357318f0451d06c54a3b0093fb652
mitchenkod <mitchenko1897@mail.ru>
2013-07-26 13:46:02 +0700

    header bug improved

---
+++ app/assets/javascripts/page.js
@@ -0,0 +1,11 @@
+$(document).ready(
+    function()
+    {
+        var nav_container = $(".nav-container");
+	    var nav = $("nav");
+
+	    var top_spacing = 0;
+
+	    nav_container.css({ 'height':nav.outerHeight() });
+	    nav.stop().addClass("sticky").css("top",-nav.outerHeight()).animate({"top":top_spacing});
+});
