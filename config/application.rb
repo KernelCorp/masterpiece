779777c8272706534973fcf29f6d19b768a6cdb9
max-konin <mk@kernel-corp.com>
2013-11-26 17:31:17 +0700

    mailing and db set up

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,14 @@ module Masterpiece
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        address:              'smtp.mail.ru',
+        port:                 465,
+        domain:               '.mail.ru',
+        user_name:            'mfc-shedevr@mail.ru',
+        password:             'shedevrladoha ',
+        authentication:       'plain',
+        enable_starttls_auto: true  }
   end
 end
