9cab86d6e27679148f22b27b34e0ccc21fef2971
max-konin <mk@kernel-corp.com>
2014-01-17 12:59:13 +0700

    brutefix

---
+++ db/migrate/20140117055507_allowed_null_fort_page_id.rb
@@ -0,0 +1,9 @@
+class AllowedNullFortPageId < ActiveRecord::Migration
+  def up
+    remove_index "refinery_page_translations", ["refinery_page_id"],
+  end
+
+  def down
+    add_index "refinery_page_translations", ["refinery_page_id"], :name => "index_refinery_page_translations_on_refinery_page_id"
+  end
+end
