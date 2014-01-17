3c38f1edba312bf92e85cc8e20a21d82b8812943
max-konin <mk@kernel-corp.com>
2014-01-17 13:11:09 +0700

    syntax error fix

--- db/migrate/20140117055507_allowed_null_fort_page_id.rb
+++ db/migrate/20140117055507_allowed_null_fort_page_id.rb
@@ -1,6 +1,6 @@
 class AllowedNullFortPageId < ActiveRecord::Migration
   def up
-    remove_index "refinery_page_translations", ["refinery_page_id"],
+    remove_index "refinery_page_translations", "refinery_page_id"
   end
 
   def down
